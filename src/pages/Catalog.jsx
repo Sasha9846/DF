8f54af7f3a23bbb63b86ee40d377166a30dc8494
abrus <a.brusentsev98@gmail.com>
2023-02-08 00:33:41 +0300

    sort okey

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,15 +1,15 @@
-import React, {useContext, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import Card from "../components/Card";
 import {Link} from "react-router-dom";
-import {EmojiFrown} from "react-bootstrap-icons"
+import {EmojiFrown, SortNumericDown, SortNumericUp} from "react-bootstrap-icons"
 import Ctx from "../Ctx";
 import Pagination from "../components/Pagination";
 import usePagination from "../hooks/usePagination";
 
 export default (data) => {
     const {visibleGoods, user, PATH} = useContext(Ctx);
-
-const paginate = usePagination(visibleGoods, 12);
+const [sortGoods, setSortGoods] = useState(visibleGoods)
+const paginate = usePagination(sortGoods, 12);
 
 const [btnType, setBtnType] = useState("");
 
@@ -19,23 +19,49 @@ let st= {
 }
 const updSort = (e) => {
     let el = e.currentTarget;
+    let flag = false;
     if (el.classList.contains("sort")) {
         el.classList.remove("sort");
         setBtnType("");
+        flag = true;
     }
     else {
         el.classList.add("sort");
         setBtnType(el.title);
     }
+    if (flag) {
+        setSortGoods(visibleGoods);
+    }
+    else {
+        let data = [...visibleGoods];
+        switch (el.title) {
+            case "down": data.sort((a, b) => a.price - b.price); break;
+            case "up": data.sort((a, b) => b.price - a.price); break;
+            case "new": data = data.filter(d => d.tags.includes("new")); break;
+            case "sale": data = data.filter(d => d.discount > 0); break;
+        }
+        setSortGoods(data);
+    }
 }
+
+useEffect(() => {
+    if (sortGoods.length === 0) {
+        setSortGoods(visibleGoods)
+    }
+    
+}, 
+[visibleGoods]);
+
+
     return <>
     {user && <>
     {visibleGoods.length > 0 
     ? <>
         <h1>Каталог товаров</h1>
+        <h3>Сортировать</h3>
         <div style={st}>
-            <button className= {`btn ${btnType === "up" ? "sort" : ""}`} title="up" onClick={updSort}>цены</button>
-            <button className= {`btn ${btnType === "down" ? "sort" : ""}`} title="down" onClick={updSort}>цены</button>
+            <button className= {`btn ${btnType === "up" ? "sort" : ""}`} title="up" onClick={updSort}>По цене &nbsp;<SortNumericDown/></button>
+            <button className= {`btn ${btnType === "down" ? "sort" : ""}`} title="down" onClick={updSort}>По цене &nbsp;<SortNumericUp/></button>
             <button className= {`btn ${btnType === "new" ? "sort" : ""}`} title="new" onClick={updSort}>Новинки</button>
             <button className= {`btn ${btnType === "sale" ? "sort" : ""}`} title="sale" onClick={updSort}>Скидки</button>
         </div>
